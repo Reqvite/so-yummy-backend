45290b646d775f21171b0b2e7b833ea431504907
Vladyslav Yachyn <104788608+Reqvite@users.noreply.github.com>
2023-04-01 00:20:20 +0300

    Update updateUserController.js

--- controllers/users/updateUserController.js
+++ controllers/users/updateUserController.js
@@ -1,36 +1,13 @@
-const path = require("path");
-
-const Jimp = require("jimp");
-
-const fs = require("fs/promises");
-
 const { User } = require("../../models/userModel");
 
 const updateUserController = async (req, res) => {
   const { _id } = req.user;
 
-  const { path: tmpUpload, originalname } = req.file;
-
-  const filename = `${_id}_${originalname}`;
-
-  const avatarsDir = path.join(__dirname, "../../public/avatars", filename);
-
-  const file = await Jimp.read(tmpUpload);
-
-  await file.resize(250, 250).writeAsync(avatarsDir);
-
-  fs.unlink(tmpUpload);
-
-  const result = await User.findOneAndUpdate({ _id }, req.body, {
-    new: true,
-  });
-  if (!result) {
-    throw new Error(404, "Not found!");
-  }
+  const { path } = req.file;
 
   const user = await User.findByIdAndUpdate(
     _id,
-    { avatarURL: avatarsDir },
+    { avatarURL: path },
     { new: true }
   );
 
@@ -38,10 +15,7 @@ const updateUserController = async (req, res) => {
     throw new Error(404, "Not found");
   }
 
-  res.json({
-    avatarURL: user.avatarURL,
-    result,
-  });
+  res.status(200).json({ status: "OK", code: 200, user });
 };
 
 module.exports = {
