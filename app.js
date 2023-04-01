a13027cf58bfd188f61f45d74528bca168a2ee2d
Viktor <jude.peper@gmail.com>
2023-04-01 20:46:14 +0200

    adds swagger documentation for all routs

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { authRouter } = require("./routes/api/authRouter");
 const { recipesRouter } = require("./routes/api/recipesRouter");
 const { errorHandler } = require("./helpers/apiHelpers");
 const { ownRecipes } = require("./routes/api/ownRecipesRouter");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
@@ -15,11 +17,10 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
 app.use("/api/users", authRouter);
 app.use("/api/recipes", recipesRouter);
 app.use("/api/own-recipes", ownRecipes);
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandler);
 
 module.exports = app;
