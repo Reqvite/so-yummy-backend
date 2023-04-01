0afd06fab6361f9003860aa74b5be42ad76fa731
Samira Esmail Pur <samiraesmailpur05@gmail.com>
2023-04-01 14:27:33 +0300

    added method sortRecipes

--- routes/api/recipesRouter.js
+++ routes/api/recipesRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/recipes/mainPage");
 
-router.get("/main-page", ctrl.listRecipes);
+router.get("/main-page", ctrl.getRecipes);
 
 module.exports = {
   recipesRouter: router,
