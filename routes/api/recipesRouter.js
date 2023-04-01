5c74065bb6b7bef1b3a797998d7ee071d2cafcbf
Uliya22 <uliya.d@ukr.net>
2023-04-01 14:49:01 +0300

    fix conflict

--- routes/api/recipesRouter.js
+++ routes/api/recipesRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { asyncWrapper } = require("../../helpers/apiHelpers");
+const ctrl = require("../../controllers/recipes/mainPage");
 const {
-  listRecipes,
   getCategoryController,
 } = require("../../controllers/recipes");
 
-router.get("/main-page", listRecipes);
+router.get("/main-page", ctrl.listRecipes);
 router.get("/category-list", asyncWrapper(getCategoryController));
 
 module.exports = {
