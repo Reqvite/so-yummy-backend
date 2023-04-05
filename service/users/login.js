cf44a365453225bc865cc1d86b8c300e6ea20140
Reqvite <reqvite2233@gmail.com>
2023-04-05 22:15:29 +0300

    Update login.js

--- service/users/login.js
+++ service/users/login.js
@@ -6,9 +6,9 @@ const { User } = require("../../models/userModel");
 const login = async (email, password) => {
   const user = await User.findOne({ email });
 
-  if (!user.subscribe) {
-    throw new NotAuthorizideError("Email not subscribed");
-  }
+  // if (!user.subscribe) {
+  //   throw new NotAuthorizideError("Email not subscribed");
+  // }
 
   if (!user) {
     throw new NotAuthorizideError("Email or password is wrong");
