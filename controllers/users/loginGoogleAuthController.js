49a2f05b0ac476f5a222f3fc4c5d332ff16665cc
Reqvite <reqvite2233@gmail.com>
2023-04-11 16:06:56 +0300

    add googleAuth

---
+++ controllers/users/loginGoogleAuthController.js
@@ -0,0 +1,19 @@
+const { NotAuthorizideError } = require("../../helpers/errors");
+const { User } = require("../../models/userModel");
+const loginGoogleAuthController = async (req, res) => {
+  const { token } = req.params;
+  if (!token) {
+    throw new NotAuthorizideError("Not Authorizide");
+  }
+
+  const user = await User.findOne(
+    { token },
+    { email: 1, name: 1, avatarURL: 1, _id: 0 }
+  );
+
+  res.status(200).json({ status: "OK", code: 200, token, user });
+};
+
+module.exports = {
+  loginGoogleAuthController,
+};
