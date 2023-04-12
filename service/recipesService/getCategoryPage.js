9b95d8417b2c5f68767c5ad859a9c7c8f8009824
Uliya22 <uliya.d@ukr.net>
2023-04-12 21:24:30 +0300

    totalCount
    Додано  totalCount для пагінації

--- service/recipesService/getCategoryPage.js
+++ service/recipesService/getCategoryPage.js
@@ -2,11 +2,12 @@ const { Recipe } = require("../../models/recipesModel");
 
 const getCategoryPage = async (category, skip, limit) => {
   const regex = new RegExp(category, "i");
+  const totalCount = await Recipe.countDocuments({ category: regex });
   const recipesPages = await Recipe.find({ category: regex })
     .sort({ popularity: -1 })
     .skip(skip)
     .limit(limit);
-  return recipesPages;
+  return [recipesPages, totalCount];
 };
 
 module.exports = {
