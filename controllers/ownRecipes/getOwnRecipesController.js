ec61fa63eb0c30b382ef8191c7946b448bbe73e5
Reqvite <reqvite2233@gmail.com>
2023-04-13 13:18:08 +0300

    Update getOwnRecipesController.js

--- controllers/ownRecipes/getOwnRecipesController.js
+++ controllers/ownRecipes/getOwnRecipesController.js
@@ -1,4 +1,3 @@
-const { Recipe } = require("../../models/recipesModel");
 const { getOwnRecipes } = require("../../service/ownRecipes/getOwnRecipes");
 
 const getOwnRecipesController = async (req, res) => {
@@ -7,26 +6,15 @@ const getOwnRecipesController = async (req, res) => {
   const limit = 4;
   const startIndex = (page - 1) * limit;
 
-  if (req.query.page === "undefined") {
-    const recipes = await Recipe.find({ owner: _id });
-    res.json({
-      status: "success",
-      code: 200,
-      recipes,
-      currentPage: 0,
-      totalPages: 0,
-    });
-  } else {
-    const { recipes, count } = await getOwnRecipes(limit, startIndex, _id);
-    console.log(2);
-    res.json({
-      status: "success",
-      code: 200,
-      recipes,
-      currentPage: page,
-      totalPages: Math.ceil(count / limit),
-    });
-  }
+  const { recipes, count } = await getOwnRecipes(limit, startIndex, _id);
+
+  res.json({
+    status: "success",
+    code: 200,
+    recipes,
+    currentPage: page,
+    totalPages: Math.ceil(count / limit),
+  });
 };
 
 module.exports = {
